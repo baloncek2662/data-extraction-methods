a4494366fc2c834dce3c3b2c3c37149283352190
Gašper Martič <martic.gasper@gmail.com>
2021-04-26 17:26:23 +0200

    finish delo scraper

--- scrapynews/scrapynews/spiders/delo_spider.py
+++ scrapynews/scrapynews/spiders/delo_spider.py
@@ -13,24 +13,57 @@ class DeloSpider(scrapy.Spider):
             
         # TODO import from ../../../constants.py - URLS_DELO
         urls = [
-            'https://www.delo.si/'
+            'https://www.delo.si/novice',
+            'https://www.delo.si/gospodarstvo',
+            'https://www.delo.si/lokalno',
+            'https://www.delo.si/mnenja',
+            'https://www.delo.si/sport',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         if self.save_files:
-            page = response.url.split("/")[-2]
-            filename = f'./scraped-content/{spider_name}-{page}.html'
+            article = response.url.split("/")[-2]
+            filename = f'./scraped-content/{spider_name}-{article}.html'
             with open(filename, 'wb') as f:
                 f.write(response.body)
 
         
-        article_names = response.xpath('//span[has-class("article_teaser__title_text")]/text()').getall()
-        def trim_element(el):
+        article_titles = self.get_all_titles(response)
+        yield {'titles' : article_titles}
+
+        # NOT IN USE - use when urls are specific articles
+        #article_data_list = self.get_article_data_list(response)
+
+    def get_all_titles(self, response):
+        article_titles = response.xpath('//span[has-class("article_teaser__title_text")]/text()').getall()
+        for i in range(len(article_titles)):
+            article_titles[i] = article_titles[i].replace('\n', '')
+            article_titles[i] = article_titles[i].strip()
+        return article_titles
+
+
+    # NOT IN USE - needs to scrape from different urls (www.delo.si)
+    def get_article_data_list(self, response):
+        title = response.xpath('//h1[has-class("article__title")]/text()').get()
+        subtitle = response.xpath('//div[has-class("article__subtitle")]/text()').get()
+        content = self.get_article_content(response)
+        image_captions_list = response.xpath('//div[has-class("article__image-caption")]/text()').getall()
+        return {
+            'title': title,
+            'subtitle': subtitle,
+            'content': content,
+            'image_captions_list': image_captions_list,
+        }
+
+    def get_article_content(self, response):
+        content_list = response.xpath('//div[has-class("article__content")]/text()').getall()
+        content = ''
+        for el in content_list:
             el = el.replace('\n', '')
             el = el.strip()
-            return el
-        article_names = map(trim_element , article_names)
-        article_names = list(article_names)
-        print(article_names)
+            if el != '':
+                content += el
+
+        return content
