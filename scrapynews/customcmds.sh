3880de1d75c9d1c6fa2d2b3cd9c51f0412c6dfb0
Gašper Martič <martic.gasper@gmail.com>
2021-06-10 22:01:38 +0200

    optimize scrapy custom commands, disable log print

--- scrapynews/customcmds.sh
+++ scrapynews/customcmds.sh
@@ -3,8 +3,9 @@
 readonly BASE_PATH=$(pwd)
 export PYTHONPATH=$PYTHONPATH:BASE_PATH/../../
 
-scrapy crawl delo -O scraped-content/delo.json -a save-files=True
-scrapy crawl rtvslo -O scraped-content/rtvslo.json -a save-files=True
-scrapy crawl zurnal -O scraped-content/zurnal.json -a save-files=True
-scrapy crawl 24ur -O scraped-content/24ur.json -a save-files=True
-scrapy crawl slovenskenovice -O scraped-content/slovenskenovice.json -a save-files=True
+SPIDER_NAMES=('delo' 'rtvslo' 'zurnal' '24ur' 'slovenskenovice')
+
+for SPIDER in $SPIDER_NAMES 
+do
+    scrapy crawl $SPIDER -O scraped-content/$SPIDER.json -a save-files=True  -s LOG_ENABLED=False
+done
